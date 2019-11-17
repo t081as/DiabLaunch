a94163e69b56a1707d74f454b8c3e7e35cd2279d
Tobias Koch <t.koch@tk-software.de>
2019-11-18 00:14:59 +0100

    Adding helper class

---
+++ build/TextFile.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Text;
+
+class TextFile
+{
+    public static void ReplaceValues(string fileName, params (string searchTerm, string replacement)[] values)
+    {
+        string content = File.ReadAllText(fileName);
+
+        foreach (var value in values)
+        {
+            content = content.Replace(value.searchTerm, value.replacement);
+        }
+
+        File.WriteAllText(fileName, content, new UTF8Encoding(false));
+    }
+}
